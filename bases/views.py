3a8bda8cffa20f38df0a20ddec3bf733074f0ce7
sergiorueda9090 <sergiorueda90@hotmail.com>
2025-06-17 16:23:51 -0500

    Mejora y tagmanager

--- bases/views.py
+++ bases/views.py
@@ -6,7 +6,7 @@ from django.core.serializers import serialize
 
 # Create your views here.
 def Home(request):
-    pages = HomePage.objects.all()
+    pages   = HomePage.objects.all()
     courses = Course.objects.all()
 
     return render(request, 'home.html', {"pages":pages, "courses":courses})
